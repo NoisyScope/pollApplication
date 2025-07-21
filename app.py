b1a785eaceb3c1f6694720dbdc670c63520f91d7
Mauricio Espinosa <mauricio.espinosa1@daikinapplied.com>
2025-07-21 12:48:28 -0600

    feat : delete votes

--- app.py
+++ app.py
@@ -14,9 +14,19 @@ GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 # Simple in-memory storage for the single poll
 poll_data = {
     'question': 'Comida de despedida',
-    'options': ['Red', 'Blue', 'Green', 'Yellow'],
-    'locations': ['', '', '', ''],  # Google Maps URLs for each option
-    'votes': [0, 0, 0, 0]  # votes for each option
+    'options': [
+        'Jijos del Mar [mariscos]',
+        'Pizzas Don Perro',
+        'Bristol Pub',
+        'Hell Fire Club'
+    ],
+    'locations': [
+        'https://www.google.com/maps/place/JIJOS+DEL+MAR/@22.1604222,-100.9988385,17z/data=!3m1!4b1!4m6!3m5!1s0x842a9fcf5f1a2427:0x88d8bfcfc0aa15ca!8m2!3d22.1604173!4d-100.9962636!16s%2Fg%2F11rc906j2c?entry=ttu&g_ep=EgoyMDI1MDcyMC4wIKXMDSoASAFQAw%3D%3D',
+        'https://www.google.com/maps/place/Pizzas+DON+PERRO+%F0%9F%94%A5/@22.0915427,-100.8776375,17z/data=!3m1!4b1!4m6!3m5!1s0x842aa58c4e29e813:0x55ca669a515a584!8m2!3d22.0915427!4d-100.8776375!16s%2Fg%2F11t5d7bhtp?entry=ttu&g_ep=EgoyMDI1MDcyMC4wIKXMDSoASAFQAw%3D%3D',
+        'https://www.google.com/maps/place/Bristol+Pub/@22.1527705,-101.0132917,17z/data=!3m1!4b1!4m6!3m5!1s0x842a9f328b58400d:0xc38a0bd0092e9af8!8m2!3d22.1527705!4d-101.0132917!16s%2Fg%2F11b6_wfy95?entry=ttu&g_ep=EgoyMDI1MDcyMC4wIKXMDSoASAFQAw%3D%3D',
+        'https://www.google.com/maps/place/Hell+Fire+Club/@22.1487223,-100.9827061,17z/data=!3m1!4b1!4m6!3m5!1s0x842aa3b744f7e0c5:0x1171a1e64255cdd2!8m2!3d22.1487174!4d-100.9801312!16s%2Fg%2F11rq0jtt5y?entry=tts&g_ep=EgoyMDI1MDcyMC4wIPu8ASoASAFQAw%3D%3D&skid=e6db5548-3a00-4cf1-8caf-7407de765258'
+    ],
+    'votes': [0, 0, 0, 0]
 }
 
 @app.route('/')
@@ -63,6 +73,12 @@ def remove_option():
         poll_data['options'].pop(option_index)
         poll_data['locations'].pop(option_index)
         poll_data['votes'].pop(option_index)
+    return redirect(url_for('manage_page'))
+
+@app.route('/resetcount', methods=['GET'])
+def reset_count():
+    # Reset the vote counts to zero
+    poll_data['votes'] = [0] * len(poll_data['options'])
     return redirect(url_for('manage_page'))
 
 if __name__ == '__main__':
